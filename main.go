b76b11a3688391dc59769eb8c64ee5122807f874
partherope <125851906@qq.com>
2023-11-14 16:43:45 +0100

    Simulate the communications in main.go

--- main.go
+++ main.go
@@ -11,14 +11,8 @@ import (
 func main() {
 	//create a manager
 	manager.GetManagerInstance("Manager1", "Location1")
-	// 创建Shop实例
 	shop1 := shop.NewShop("Shop1", "Location1", 3)
 	shop1.AddEvent("Test2")
-	// 打印Shop实例以确认创建
 	fmt.Println("Created shops:", shop1.GetShopID())
-
 	time.Sleep(10 * time.Second)
-
-	// 在测试结束后关闭计时器，可选
-	// shop.StopTicker() // 假设您在shop包中有这样一个方法
 }
