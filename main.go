e54ad452ad9672d84104dbe677eed4384c18e326
Zikang CHEN <65004114+Appointat@users.noreply.github.com>
2023-11-22 23:20:47 +0100

    Refactor outlet creation and event handling

--- main.go
+++ main.go
@@ -13,17 +13,70 @@ func main() {
 	centralHub := centralhub.GetHubInstance("Central_Hub1", "Location1")
 
 	// Create an outlet
-	outlet1 := outlet.NewOutlet("Outlet1", "Location1", 3)
-	outlet1.AddEvent("Test2")
-
-	// Output the created outlet information
-	fmt.Println("Created outlet:", outlet1.GetOutletID())
+	numberOutlets := 5
+	outlets := make([]*outlet.Outlet, numberOutlets)
+	for i := 0; i < numberOutlets; i++ {
+		location := fmt.Sprintf("Location %d", i)
+		outletID := fmt.Sprintf("Outlet %d", i)
+		outlets[i] = outlet.NewOutlet(outletID, location, 3, holidayMaps[i])
+		fmt.Println("Created outlet:", outlets[i].GetOutletID())
+	}
 
 	// Sleep to allow for background processes to run
-	time.Sleep(10 * time.Second)
+	time.Sleep(10 * time.Second) // TODO: Keep main function running
 
 	// Assuming there might be additional functionality to print or check the status
-	// This could include printing the current date or events
-	fmt.Println("Current Date:", outlet.GetCurrentDate())
 	fmt.Printf("Central Hub: %+v\n", centralHub)
+}
+
+// HolidayEvents maps event names to their dates.
+var holidayMaps = []map[string]time.Time{
+	{
+		"New Year's Day":    time.Date(2023, 1, 1, 0, 0, 0, 0, time.UTC),
+		"Valentine's Day":   time.Date(2023, 2, 14, 0, 0, 0, 0, time.UTC),
+		"St. Patrick's Day": time.Date(2023, 3, 17, 0, 0, 0, 0, time.UTC),
+		// ... (other events) ...
+		"Chinese National Day": time.Date(2023, 10, 1, 0, 0, 0, 0, time.UTC),
+		"Event_11_25":          time.Date(2023, 11, 25, 0, 0, 0, 0, time.UTC),
+		"Event_11_26":          time.Date(2023, 11, 26, 0, 0, 0, 0, time.UTC),
+	},
+	// Copy the same map for the sake of this example
+	{
+		"New Year's Day":    time.Date(2023, 1, 1, 0, 0, 0, 0, time.UTC),
+		"Valentine's Day":   time.Date(2023, 2, 14, 0, 0, 0, 0, time.UTC),
+		"St. Patrick's Day": time.Date(2023, 3, 17, 0, 0, 0, 0, time.UTC),
+		// ... (other events) ...
+		"Chinese National Day": time.Date(2023, 10, 1, 0, 0, 0, 0, time.UTC),
+		"Event_11_25":          time.Date(2023, 11, 25, 0, 0, 0, 0, time.UTC),
+		"Event_11_26":          time.Date(2023, 11, 26, 0, 0, 0, 0, time.UTC),
+	},
+	// Repeat the same map 3 more times to simulate different maps
+	// In real usage, these would likely be different
+	{
+		"New Year's Day":    time.Date(2023, 1, 1, 0, 0, 0, 0, time.UTC),
+		"Valentine's Day":   time.Date(2023, 2, 14, 0, 0, 0, 0, time.UTC),
+		"St. Patrick's Day": time.Date(2023, 3, 17, 0, 0, 0, 0, time.UTC),
+		// ... (other events) ...
+		"Chinese National Day": time.Date(2023, 10, 1, 0, 0, 0, 0, time.UTC),
+		"Event_11_25":          time.Date(2023, 11, 25, 0, 0, 0, 0, time.UTC),
+		"Event_11_26":          time.Date(2023, 11, 26, 0, 0, 0, 0, time.UTC),
+	},
+	{
+		"New Year's Day":    time.Date(2023, 1, 1, 0, 0, 0, 0, time.UTC),
+		"Valentine's Day":   time.Date(2023, 2, 14, 0, 0, 0, 0, time.UTC),
+		"St. Patrick's Day": time.Date(2023, 3, 17, 0, 0, 0, 0, time.UTC),
+		// ... (other events) ...
+		"Chinese National Day": time.Date(2023, 10, 1, 0, 0, 0, 0, time.UTC),
+		"Event_11_25":          time.Date(2023, 11, 25, 0, 0, 0, 0, time.UTC),
+		"Event_11_26":          time.Date(2023, 11, 26, 0, 0, 0, 0, time.UTC),
+	},
+	{
+		"New Year's Day":    time.Date(2023, 1, 1, 0, 0, 0, 0, time.UTC),
+		"Valentine's Day":   time.Date(2023, 2, 14, 0, 0, 0, 0, time.UTC),
+		"St. Patrick's Day": time.Date(2023, 3, 17, 0, 0, 0, 0, time.UTC),
+		// ... (other events) ...
+		"Chinese National Day": time.Date(2023, 10, 1, 0, 0, 0, 0, time.UTC),
+		"Event_11_25":          time.Date(2023, 11, 25, 0, 0, 0, 0, time.UTC),
+		"Event_11_26":          time.Date(2023, 11, 26, 0, 0, 0, 0, time.UTC),
+	},
 }
