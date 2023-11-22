7ff78e44558a7b8cf6d0925ea6d65a7c7d25bff1
Zikang CHEN <65004114+Appointat@users.noreply.github.com>
2023-11-22 20:47:49 +0100

    Refactor code to use central hub and outlet

--- main.go
+++ main.go
@@ -4,15 +4,26 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/Appointat/Responsive-AI-Clusters-in-Supply-Chain/manager"
-	"github.com/Appointat/Responsive-AI-Clusters-in-Supply-Chain/shop"
+	centralhub "github.com/Appointat/Responsive-AI-Clusters-in-Supply-Chain/central_hub"
+	"github.com/Appointat/Responsive-AI-Clusters-in-Supply-Chain/outlet"
 )
 
 func main() {
-	//create a manager
-	manager.GetManagerInstance("Manager1", "Location1")
-	shop1 := shop.NewShop("Shop1", "Location1", 3)
-	shop1.AddEvent("Test2")
-	fmt.Println("Created shops:", shop1.GetShopID())
+	// Create a central hub instance
+	centralHub := centralhub.GetHubInstance("Central_Hub1", "Location1")
+
+	// Create an outlet
+	outlet1 := outlet.NewOutlet("Outlet1", "Location1", 3)
+	outlet1.AddEvent("Test2")
+
+	// Output the created outlet information
+	fmt.Println("Created outlet:", outlet1.GetOutletID())
+
+	// Sleep to allow for background processes to run
 	time.Sleep(10 * time.Second)
+
+	// Assuming there might be additional functionality to print or check the status
+	// This could include printing the current date or events
+	fmt.Println("Current Date:", outlet.GetCurrentDate())
+	fmt.Printf("Central Hub: %+v\n", centralHub)
 }
