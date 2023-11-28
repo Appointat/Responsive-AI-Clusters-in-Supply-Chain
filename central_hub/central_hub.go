4092492da298b1fa8a37346e6696c3921cdd7ecb
partherope <125851906@qq.com>
2023-11-28 17:31:20 +0100

    Integrate AI response to general info and handle WebSocket connection

--- central_hub/central_hub.go
+++ central_hub/central_hub.go
@@ -5,10 +5,13 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"sync"
+	"time"
 
 	"github.com/Appointat/Responsive-AI-Clusters-in-Supply-Chain/product"
+	"github.com/gorilla/websocket"
 )
 
 type ProductInfo struct {
@@ -24,17 +27,32 @@ type AIRequest struct {
 	ShopInventory map[string]ProductInfo `json:"shopInventory"`
 }
 
-type AIResponse struct {
-	Reply             string                 `json:"reply"`
-	DelayDays         int                    `json:"delayDays"`
-	ReplenishmentData map[string]ProductInfo `json:"replenishmentDates"`
-}
-
 /*
 ***************************************************
 To be combined with type Response in central_hub.go
 ***************************************************
 */
+type AIResponse struct {
+	Reply             string         `json:"reply"`
+	DelayDays         int            `json:"delayDays"`
+	ReplenishmentData map[string]int `json:"replenishmentDates"`
+}
+
+// GeneralInfo, SupermarketInfo, interact with the Frontend
+type GeneralInfo struct {
+	Date             time.Time      `json:"date"`
+	Event            string         `json:"event"`
+	WarehouseProduct map[string]int `json:"warehouseProduct"`
+}
+
+type SupermarketInfo struct {
+	ID           string         `json:"id"`
+	ProductLeft  map[string]int `json:"productLeft"` // The number of products left in the supermarket
+	ProductAdd   map[string]int `json:"productAdd"`  // The number of products added to the supermarket
+	DeliveryTime int            `json:"deliveryTime"`
+}
+
+// End of the part to integrate with Frontend
 type Response struct {
 	Replenishments map[string]int // Key is the name of the product, value is the number of products to replenish
 	Error          error
@@ -46,6 +64,8 @@ type CentralHub struct {
 	resources   map[string]*product.Product
 	accessLock  sync.Mutex
 	aiServerURL string
+	wsupgrader  websocket.Upgrader
+	client      *websocket.Conn
 }
 
 var (
@@ -122,8 +142,52 @@ func (h *CentralHub) SendRequestToAI(requestData AIRequest) (*AIResponse, error)
 	return &response, nil
 }
 
+// Send the general information to the frontend
+func (h *CentralHub) IntegrateAIResponseToGeneralInfo(event string, date time.Time, aiResponseData *AIResponse) *GeneralInfo {
+	// Integrate the data from AI
+
+	// Extract the info from aiResponse
+	warehouseProduct := aiResponseData.ReplenishmentData
+	generalInfo := GeneralInfo{
+		Date:             date,
+		Event:            event,
+		WarehouseProduct: warehouseProduct,
+	}
+
+	return &generalInfo
+}
+
+func (h *CentralHub) sendGeneralInfoToFrontEnd(info *GeneralInfo) {
+	// Send the general information to the frontend
+
+	// Code as JSON
+	jsonData, err := json.Marshal(info)
+	if err != nil {
+		fmt.Println("Error in sending general info to front end:", err)
+		return
+	}
+
+	// Send Post Request to Frontend
+	err = h.client.WriteMessage(websocket.TextMessage, jsonData)
+	if err != nil {
+		log.Printf("Error sending message to WebSocket: %v\n", err)
+	}
+}
+
+func (h *CentralHub) HandleWebSocket(w http.ResponseWriter, r *http.Request) {
+	// Upgrade the HTTP connection to a websocket connection
+	conn, err := h.wsupgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println("Failed to set websocket upgrade: %+v", err)
+		return
+	}
+	h.client = conn
+	defer conn.Close()
+	// Need to hangup the connection in the main function
+}
+
 // HandleEventNotification responds to an event by determining necessary product replenishments
-func (h *CentralHub) HandleEventNotification(event string, shopInventory map[string]*product.Product) *Response {
+func (h *CentralHub) HandleEventNotification(event string, date time.Time, shopInventory map[string]*product.Product) *Response {
 	h.accessLock.Lock()
 	defer h.accessLock.Unlock()
 	inventoryInfo := make(map[string]ProductInfo)
@@ -151,17 +215,19 @@ func (h *CentralHub) HandleEventNotification(event string, shopInventory map[str
 			Error:          err,
 		}
 	}
-
-	//
+	//Got Response from AI, send the general information to the frontend
+	h.sendGeneralInfoToFrontEnd(h.IntegrateAIResponseToGeneralInfo(event, date, aiResponse))
+	//Extract the info from aiResponse
 	replenishments := make(map[string]int)
 
 	// Calculate the number of products that need to be replenished
 	for name, _ := range shopInventory {
-		if replenishInfo, exists := aiResponse.ReplenishmentData[name]; exists {
-			quantityNeeded := replenishInfo.Quantity
+		if ReplenishmentData, exists := aiResponse.ReplenishmentData[name]; exists {
+			quantityNeeded := ReplenishmentData
 			if quantityNeeded != 0 {
 				replenishments[name] = quantityNeeded
-				h.resources[name].SetNumber(h.resources[name].GetNumber() + quantityNeeded)
+				//decrease the stock of the product in the central hub
+				h.resources[name].SetNumber(h.resources[name].GetNumber() - quantityNeeded)
 			} else {
 				replenishments[name] = 0
 			}
