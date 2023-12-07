e5be27a23a1502e3afe9115311750c4f151cc3f7
partherope <125851906@qq.com>
2023-12-08 00:38:51 +0100

    Fix event struct initialization in HolidayMaps

--- event/event.go
+++ event/event.go
@@ -9,12 +9,13 @@ type Event struct {
 	EventDate        time.Time
 }
 
-var HolidayMaps = []map[string]Event{
+var HolidayMaps = []map[string]*Event{
 	//EVENTS for outlet1
-	{"Christmas": Event{
-			EventDescription: "The upcoming Lavender Festival in the region is expected to significantly increase the demand for local specialties. We anticipate a higher demand for Olive Oil and Baguette as tourists prefer local culinary experiences. Preparing additional stock of these items is advised to meet the increased customer flow."
+	{
+		"Christmas": &Event{
+			EventDescription: "The upcoming Lavender Festival in the region is expected to significantly increase the demand for local specialties. We anticipate a higher demand for Olive Oil and Baguette as tourists prefer local culinary experiences. Preparing additional stock of these items is advised to meet the increased customer flow.",
 			EventDate:        time.Date(2023, 12, 25, 0, 0, 0, 0, time.UTC),
-	},
+		},
 	},
 	//EVENTS for outlet2
 
