ded54541c3bd5f3c951bd6b162008139f1807c07
Zikang CHEN <65004114+Appointat@users.noreply.github.com>
2023-12-26 13:54:26 +0100

    Add message queue and fix websocket in send_streaming_message

--- back_end/ai/app.py
+++ back_end/ai/app.py
@@ -1,3 +1,4 @@
+import queue
 from flask import Flask, request, jsonify
 import asyncio
 import websockets
@@ -9,8 +10,9 @@ import logging
 
 from multi_agent_communication_supply_chain import role_playing
 
-
 global messages_queue, central_hub_json
+
+messages_queue = queue.Queue()
 
 central_hub_json = {
     "central_hub_inventory": {
@@ -76,17 +78,14 @@ app = Flask(__name__)
 #             await asyncio.sleep(1)
 
 #     except websockets.exceptions.ConnectionClosedError as e:
-#         logging.error(f"WebSocket connection closed: {e}")
+#         logging.error("WebSocket connection closed error", e)
+#     except Exception as e:
+#         logging.error("Error in send_streaming_message", e)
 
-async def get_message_from_queue(q):
-    loop = asyncio.get_running_loop()
-    import concurrent.futures
-    with concurrent.futures.ThreadPoolExecutor() as pool:
-        # Run a blocking function in the background
-        message = await loop.run_in_executor(pool, q.get)
-        return message
+async def get_message_from_queue(messages_queue):
+    return await asyncio.to_thread(messages_queue.get)
 
-async def send_streaming_message(websocket, path, messages_queue):
+async def send_streaming_message(websocket, path):
     print(f"messages_queue, send_streaming_message:\n{messages_queue}")
 
     while True:
@@ -117,11 +116,11 @@ async def send_streaming_message(websocket, path, messages_queue):
             await websocket.send(json.dumps(msg_to_send))
 
         messages_queue.task_done()  # Mark the task as done
- 
+
 def run_websocket_server(messages_queue):
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
-    start_server = websockets.serve(functools.partial(send_streaming_message, messages_queue=messages_queue), 'localhost', 8000)
+    start_server = websockets.serve(functools.partial(send_streaming_message), 'localhost', 8000)
     loop.run_until_complete(start_server)
     loop.run_forever()
 
@@ -131,31 +130,32 @@ current_messages_queue = None
 # Define a route for the AI request
 @app.route('/ai', methods=['POST'])
 def handle_ai_request():
-    global websocket_server_thread
-    global current_messages_queue
-
     # Get JSON data from the request
     request_data = request.get_json()
 
-    # Perform some AI-related processing here...
+    # Perform some AI-related processing with role_playing
     global central_hub_json
-    response_json, _central_hub_json, new_messages_queue = role_playing(request_json=request_data, central_hub_json=central_hub_json)
-    central_hub_json = _central_hub_json
-    print(f"messages_queue, handle_ai_request:\n{new_messages_queue}")
+    response_json, updated_central_hub_json = role_playing(request_json=request_data, central_hub_json=central_hub_json)
+    central_hub_json = updated_central_hub_json
 
-    # Start WebSocket server in a separate thread if not already running
-    current_messages_queue = new_messages_queue
-    if websocket_server_thread is None or not websocket_server_thread.is_alive():
-        websocket_server_thread = threading.Thread(target=run_websocket_server, args=(current_messages_queue,), daemon=True)
-        websocket_server_thread.start()
-
-    # Send JSON response
+    # Return the response from role_playing
     return jsonify(response_json)
 
 
-if __name__ == '__main__':
-    # Configure logging
-    logging.basicConfig(level=logging.INFO)
+@app.route('/send/<message>')
+def send_message(message):
+    messages_queue.put(message)
+    return 'Message enqueued'
 
+def run_flask_app():
     # Running on http://0.0.0.0:5000/ without threading even in debug mode
-    app.run(debug=True, host='0.0.0.0', port=5000, threaded=False)
+    app.run(debug=True, host='0.0.0.0', port=5000)
+
+def main():
+    flask_thread = threading.Thread(target=run_flask_app)
+    flask_thread.start()
+
+    run_websocket_server()
+
+if __name__ == "__main__":
+    main()
