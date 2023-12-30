7716754e8c18e9afb398a49934c7b9a525ab2c03
Zikang CHEN <65004114+Appointat@users.noreply.github.com>
2023-12-30 01:22:29 +0100

    Fix bug in login functionality
    Update README with new instructions
    
    Refactor code for better performance
    
    Add new feature for user authentication
    
    Fix formatting issues in code
    
    Update dependencies to latest versions
    
    Remove unused variables
    
    Fix broken links in documentation
    
    Add error handling for edge cases
    
    Optimize database queries
    
    Update CSS styles for better user experience

--- back_end/ai/multi_agent_communication_supply_chain.py
+++ back_end/ai/multi_agent_communication_supply_chain.py
@@ -3,16 +3,18 @@ import queue
 
 from colorama import Fore
 
-from camel.configs import ChatGPTConfig
+from camel.configs import ChatGPTConfig, FunctionCallingConfig
 from camel.societies import RolePlaying
 from camel.utils import print_text_animated
-from camel.functions import MATH_FUNCS, SEARCH_FUNCS
+from camel.functions import MATH_FUNCS
 from camel.types import ModelType, TaskType
+
+from format_agent import FormatAgent
 
 messages_queue = queue.Queue()
 
 
-def role_playing(model_type=ModelType.GPT_3_5_TURBO_16K, chat_turn_limit=50, request_json=None, central_hub_json=None) -> None:
+def role_playing(model_type=ModelType.GPT_3_5_TURBO, chat_turn_limit=50, request_json=None, central_hub_json=None) -> None:
     if request_json is None:
         request_json = {
             "outlet_id": "1",
@@ -47,7 +49,7 @@ def role_playing(model_type=ModelType.GPT_3_5_TURBO_16K, chat_turn_limit=50, req
             }
         }
     user_id = request_json["outlet_id"]
-    post_outlet_inventory_json = request_json['outlet_inventory']
+    post_outlet_inventory_json = request_json["outlet_inventory"]
     response_json = {
         "outlet_inventory": {
             "olive_oil": {
@@ -79,12 +81,15 @@ The \"specific_reason_of_replenishment\" means the specific reason of replenishm
 THe current storage amount of the outlet should be less than the maximum capacity of the warehouse of the outlet.
 While making decisions, the central hub should first consider the neccessary information in the context, and then predict what is the unknown demand of outlet in the event.
 """
-    task_prompt = "In order to help the outlet to handle the upcoming events well," + \
-        " please make decisions based on the known information (you need to show the basis and the thoughts specifically). And finally before the \"CAMEL_TASK_DONE\", the AI assistant (Event Logistics Coordinator of Outlet) MUST strictly adhere to the structure of the JSON TEMPLATE, ONLY fill in the BLANKs, and DO NOT alter or modify any other part of the template.\n"
+    task_prompt = "In order to help the outlet to handle the upcoming events well, " + \
+        "please make decisions based on the known information (you need to show the basis and the thoughts specifically). "
+        # "And finally before the \"CAMEL_TASK_DONE\", the AI assistant (Event Logistics Coordinator of Outlet) MUST strictly adhere to the structure of the JSON TEMPLATE, ONLY fill in the BLANKs, and DO NOT alter or modify any other part of the template.\n"
     assistant_answer_template = "Even if some BLANKs/NUMs/STRINGs are not moentioned (for example, \"specific_reason_of_replenishment\": \"<BLANCK>\") in the conversation, you must fill them with sertain values or strings.\n"
     answer_template = "===== JSON TEMPLATE =====\n"
     answer_template += json.dumps(response_json, indent=4)
     assistant_answer_template += answer_template
+
+    chat_recode = context_text
 
     ai_user_role = "Inventory Management Specialist of Central Hub"
     ai_user_description = "This expert should have strong organizational skills, attention to detail, and a deep understanding of supply chain and inventory management systems. They should be proficient in inventory tracking software and have the ability to analyze stock levels to ensure availability for events. Their duties would include categorizing goods, forecasting demand based on the event description, and configuring the inventory system to reflect accurate information for event-specific requirements."
@@ -92,12 +97,11 @@ While making decisions, the central hub should first consider the neccessary inf
     ai_assistant_description = "The expert must have experience in event planning and logistics, with a knack for coordinating with multiple stakeholders. They should have competencies in project management, communication, and problem-solving. Their duties involve understanding the event description to determine the necessary goods, liaising with the Inventory Management Specialist to ensure proper stock levels, and overseeing the setup to meet the event's needs."
 
     # You can use the following code to play the role-playing game
-    function_list = [*MATH_FUNCS, *SEARCH_FUNCS]
-    # assistant_model_config = FunctionCallingConfig.from_openai_function_list(
-    #     function_list=function_list,
-    #     kwargs=dict(temperature=0.0),
-    # )
-    assistant_model_config = ChatGPTConfig(temperature=0.0)
+    function_list = [*MATH_FUNCS]
+    assistant_model_config = FunctionCallingConfig.from_openai_function_list(
+        function_list=function_list,
+        kwargs=dict(temperature=0.0),
+    )
     user_model_config = ChatGPTConfig(temperature=0.0)
     sys_msg_meta_dicts = [
         dict(
@@ -126,9 +130,6 @@ While making decisions, the central hub should first consider the neccessary inf
     )
     print(Fore.YELLOW + f"Original task prompt:\n{task_prompt}\n")
  
-
-    final_answer_json = None
-
     n = 0
     input_assistant_msg, _ = role_play_session.init_chat()
     while n < chat_turn_limit:
@@ -155,66 +156,65 @@ While making decisions, the central hub should first consider the neccessary inf
         messages_queue.put({"sender_id": user_id, "user_message": user_response.msg.content, "assistant_message": assistant_response.msg.content})
         print(Fore.WHITE + f"The length of the messages_queue is {messages_queue.qsize()}\n")
 
-        match_and_replace = lambda a, b: {k: match_and_replace(a[k], b[k]) if isinstance(a[k], dict) else b[k] for k in a}
-
-        # Match the values in the response with the request
-        try:
-            _final_answer_json = None
-            match = re.search(r'\{.*\}', user_response.msg.content, re.DOTALL)
-            if match is not None:
-                json_string = match.group()
-                parsed_json = json.loads(json_string)
-                _final_answer_json = match_and_replace(response_json, parsed_json)
-        except:
-            warning = "The user response is not in the correct format. Please check the user response."
-        if _final_answer_json is not None:
-            final_answer_json = _final_answer_json
-
-        try:
-            _final_answer_json = None
-            match = re.search(r'\{.*\}', assistant_response.msg.content, re.DOTALL)
-            if match is not None:
-                json_string = match.group()
-                parsed_json = json.loads(json_string)
-                _final_answer_json = match_and_replace(response_json, parsed_json)
-        except:
-            warning = "The assistant response is not in the correct format. Please check the assistant response."
-        if _final_answer_json is not None:
-            final_answer_json = _final_answer_json
+        chat_recode += (f"[{ai_user_role}]:{user_response.msg.content}\n\n" + \
+            f"[{ai_assistant_role}]:{assistant_response.msg.content}\n\n")
 
         if "CAMEL_TASK_DONE" in user_response.msg.content or \
             "CAMEL_TASK_DONE" in assistant_response.msg.content:
+
+            format_agent = FormatAgent(model_type=ModelType.GPT_4_TURBO, model_config=ChatGPTConfig(temperature=0.0))
+            output_text = format_agent.run(
+                user_role_name=ai_user_role,
+                assistant_role_name=ai_assistant_role,
+                chat_record=chat_recode,
+                answer_template=response_json,
+            ).replace("\'", "\"")
+            role_playing_output_json = json.loads(output_text)
+            role_playing_output_json["transportation_duration"] = [int(s) for s in role_playing_output_json["transportation_duration"].split() if s.isdigit()][0]
+            print(Fore.RED + f"output_json:\n{json.dumps(role_playing_output_json, indent=4)}\n")
+            # {
+            #     "outlet_inventory": {
+            #         "olive_oil": {
+            #             "future_storage_amount": "150",
+            #             "specific_reason_of_replenishment": "Expected high demand for Bastille Day event"
+            #         },
+            #         "baguette": {
+            #             "future_storage_amount": "250",
+            #             "specific_reason_of_replenishment": "Moderate demand expected for Bastille Day event"
+            #         },
+            #         "manchego_cheese": {
+            #             "future_storage_amount": "400",
+            #             "specific_reason_of_replenishment": "Strong preference and high demand expected for Bastille Day event"
+            #         },
+            #         "black_tea": {
+            #             "future_storage_amount": "50",
+            #             "specific_reason_of_replenishment": "Minimal interest expected for Bastille Day event"
+            #         }
+            #     },
+            #     "transportation_duration": 2
+            # }
             break
 
         input_assistant_msg = assistant_response.msg
 
     # Convert string into JSON
-    outlet_inventory_json = final_answer_json['outlet_inventory']
+    outlet_inventory_json = role_playing_output_json["outlet_inventory"]
+    trasportation_duration_json = role_playing_output_json["transportation_duration"]
 
     # Calculate the changed replenishment amount from central hub
-    changed_replenishment_amount_from_central_hub = {}
-    for item in outlet_inventory_json:
-        if item in post_outlet_inventory_json:
-            # Subtract the values of the same items
-            changed_replenishment_amount_from_central_hub[item] = {
-                "changed_replenishment_amount_from_central_hub":
-                int(outlet_inventory_json[item]["future_storage_amount"]) -
-                int(post_outlet_inventory_json[item]["current_storage_amount"])
-            }
-            # Add the values of the same items
-            central_hub_json["central_hub_inventory"][item]["current_storage_amount"] -= \
-                changed_replenishment_amount_from_central_hub[item]["changed_replenishment_amount_from_central_hub"]
+    for product in outlet_inventory_json:
+        current_storage_amount = request_json["outlet_inventory"][product]["current_storage_amount"]
+        future_storage_amount = outlet_inventory_json[product]["future_storage_amount"]
+        changed_replenishment_amount_from_central_hub = int(future_storage_amount) - int(current_storage_amount)
+        if changed_replenishment_amount_from_central_hub < 0:
+            changed_replenishment_amount_from_central_hub = 0
+        central_hub_json["central_hub_inventory"][product]["current_storage_amount"] -= changed_replenishment_amount_from_central_hub
 
-    duration_str = final_answer_json.get("transportation_duration", "1 day")  # Default to '1 day' if not found
-    # Find all numbers in the string (assuming the first one is the day count)
-    numbers = [int(s) for s in duration_str.split() if s.isdigit()]
-    transportation_duration_json = {}
-    transportation_duration_json["transportation_duration"] = numbers[0] if len(numbers) > 0 else 1
-
+    # Format the final answer json
     final_answer_json = {
         "outlet_inventory": changed_replenishment_amount_from_central_hub,
         "central_hub_inventory": central_hub_json,
-        "transportation_duration": transportation_duration_json["transportation_duration"]
+        "transportation_duration": trasportation_duration_json
     }
 
     print(Fore.RED + f"final_answer_json:\n{json.dumps(final_answer_json, indent=4)}\n")
