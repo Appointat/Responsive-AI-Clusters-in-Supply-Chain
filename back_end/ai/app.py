5399a3d56abcba3ed33b83c8b7541a084a2d0a55
Zikang CHEN <65004114+Appointat@users.noreply.github.com>
2023-12-31 21:08:59 +0100

    Add default response for role_playing function failure

--- back_end/ai/app.py
+++ back_end/ai/app.py
@@ -132,7 +132,58 @@ def handle_ai_request():
     try:
         response_json, updated_central_hub_json = role_playing(request_json=request_data, central_hub_json=central_hub_json)
     except:
-        response_json, updated_central_hub_json = role_playing(request_json=request_data, central_hub_json=central_hub_json)
+        # If the role_playing function fails, return a default response
+        response_json = {
+            "outlet_inventory": {
+                "baguette": {
+                    "future_storage_amount": 50,
+                    "specific_reason_of_replenishment": "to meet the moderate demand as per the client\"s preferences"
+                },
+                "black_tea": {
+                    "future_storage_amount": 20,
+                    "specific_reason_of_replenishment": "to maintain a minimal stock level due to the client\"s minimal interest"
+                },
+                "manchego_cheese": {
+                    "future_storage_amount": 40,
+                    "specific_reason_of_replenishment": "to meet the strong demand as per the client\"s preferences"
+                },
+                "olive_oil": {
+                    "future_storage_amount": 30,
+                    "specific_reason_of_replenishment": "to meet the strong demand as per the client\"s preferences"
+                }
+            },
+            "central_hub_inventory": {
+                "baguette": {
+                    "current_storage_amount": 530
+                },
+                "black_tea": {
+                    "current_storage_amount": 364
+                },
+                "manchego_cheese": {
+                    "current_storage_amount": 530
+                },
+                "olive_oil": {
+                    "current_storage_amount": 180
+                }
+            },
+            "transportation_duration": 1
+        }
+        updated_central_hub_json = {
+            "central_hub_inventory": {
+                "baguette": {
+                    "current_storage_amount": 530
+                },
+                "black_tea": {
+                    "current_storage_amount": 364
+                },
+                "manchego_cheese": {
+                    "current_storage_amount": 530
+                },
+                "olive_oil": {
+                    "current_storage_amount": 180
+                }
+            }
+        }
 
     for product in updated_central_hub_json["central_hub_inventory"]:
         product_account = int(updated_central_hub_json["central_hub_inventory"][product]["current_storage_amount"])
